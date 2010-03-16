214ae99b26e84b3d07c3ce653126d03bef68d164
Manfred Moitzi <mozman@gmx.at>
2010-03-16 20:39:49 +0100

    refactored makedxfexamples.py

--- makedxfexamples.py
+++ makedxfexamples.py
@@ -7,40 +7,67 @@
 # License: GPLv3
 
 import math
+from random import random
+
 import dxfwrite
 
-def empty_dxf(engine, name):
-    drawing = engine.drawing(name)
+def empty_dxf(dxf, name):
+    """ create an empty drawing """
+    # create the drawing
+    drawing = dxf.drawing(name)
+    # and save it
     drawing.save()
 
-def simple_dxf(engine, name):
-    def add_all_colors(maxcolor):
-        for color in xrange(maxcolor):
-            drawing.add(engine.line((50, color), (55, color), color=color))
-            drawing.add(engine.text("color index: {0}".format(color), (56, color), .35))
+def simple_dxf(dxf, name):
+    def dxflist_example(x=0, y=0, w=1, h=1):
+        # dxf list can contain any dxf entity, that supports __dxf__()
+        rect = dxfwrite.DXFList()
+        rect.append(dxf.line(start=(x, y), end=(x+w, y), color=1))
+        rect.append(dxf.line(start=(x+w, y), end=(x+w, y+h), color=2))
+        rect.append(dxf.line(start=(x+w, y+h), end=(x, y+h), color=3))
+        rect.append(dxf.line(start=(x, y+h), end=(x, y), color=4))
+        return rect
 
-    drawing = engine.drawing(name)
+    # create a new drawing
+    drawing = dxf.drawing(name)
+    # add a LAYER-tableentry called 'dxfwrite'
     drawing.add_layer('dxfwrite')
+    # add a VIEWPORT-tableentry
     drawing.add_viewport(
             '*Active',
             center_point=(10,10),
             height = 30,
         )
-    drawing.add(engine.line(
-        start=(0,0),
-        end=(10,0),
+
+    # add LINE-entity
+    drawing.add(dxf.line((0,0),( 10,0),
         color=dxfwrite.const.BYLAYER,
         layer='dxfwrite'
     ))
-    add_all_colors(15)
-    drawing.add(engine.circle(center=(5,0), radius=5))
-    drawing.add(engine.arc(center=(5,0), radius=4, startangle=30, endangle=150))
-    drawing.add(engine.point(point=(1,1)))
-    drawing.add(engine.solid([(0,0), (1,0), (1,1), (0,1)], color=2))
-    drawing.add(engine.solid([(0,1), (1,1), (1,2)], color=3))
-    drawing.add(engine.face3d([(5,5), (6,5), (6,6), (5,6)], color=3))
-    drawing.add(engine.text(text="Manfred"))
-    drawing.add(engine.text(
+
+    # add a CIRCLE-entity
+    drawing.add(dxf.circle(center=(5,0), radius=5))
+
+    # add an ARC-entity
+    drawing.add(dxf.arc(center=(5,0), radius=4, startangle=30, endangle=150))
+
+    #add a POINT-entity
+    drawing.add(dxf.point(point=(1,1)))
+
+    # add a SOLID-entity with 4 points
+    drawing.add(dxf.solid([(0,0), (1,0), (1,1), (0,1)], color=2))
+
+    # add a SOLID-entity with 3 points
+    drawing.add(dxf.solid([(0,1), (1,1), (1,2)], color=3))
+
+    # add a 3DFACE-entity
+    drawing.add(dxf.face3d([(5,5), (6,5), (6,6), (5,6)], color=3))
+
+    # add a TEXT-entity
+    drawing.add(dxf.text(text="Manfred"))
+
+    # add a TEXT-entity with more properties
+    drawing.add(dxf.text(
         text="mozman",
         style="ISOCPEUR",
         height=0.7,
@@ -49,182 +76,152 @@ def simple_dxf(engine, name):
         insert=(0,5),
         rotation=30,
     ))
-    # create block definition
-    block = engine.block(name='Rechteck')
-    block.add(rectangle(engine, 0, 0, 1, 1))
-    attdef = engine.attdef(
+
+    # create BLOCK-entity
+    block = dxf.block(name='Rechteck')
+    # add DXF-entities to the block
+    block.add(dxflist_example(0, 0, 1, 1))
+    # create an ATTDEF-entity, can be use to crate new ATTRIBS with following
+    # default values, see attdef.new_attrib() call below
+    attdef = dxf.attdef(
         insert=(.2, .2),
         rotation = 30,
         height=0.25,
         text='test',
-        prompt='test eingeben:',
+        prompt='test eingeben:', # only important for interactive CAD systems
         tag='TEST'
     )
+    # add attdef to the block definition
     block.add(attdef)
+    # add the block to the BLOCKS section
+    drawing.blocks.add(block)
 
-    drawing.blocks.add(block) # add block to blocks section
-    for x in range(1, 10): # now we can insert a block reference
-        block_ref = engine.insert(
+    # insert the block references
+    for x in range(1, 10):
+        block_ref = dxf.insert(
             blockname='Rechteck',
             insert=(x*2,10),
             rotation=x*6,
             xscale=x,
             yscale=x)
 
-        # add an attribute
+        # create an ATTRIB-entity from an ATTDEF-entity
         attrib = attdef.new_attrib(
             height=0.18,
             text='attrib:{0}'.format(x),
         )
+
+        # add ATTRIB-entity to the block refernce
         # relative insert, respects the block rotation
         block_ref.add(attrib, relative=True)
+        # add block reference to drawing
         drawing.add(block_ref)
 
-    drawing.add(polyline(engine))
-    drawing.add(polymesh(engine))
-    drawing.add(cube(engine, basepoint=(-5, 0, 3), length=7))
-    drawing.add(engine.line((0,-3), (3,-3)))
-    drawing.add(engine.text("aligned Text", insert=(0, -3),
+    # example for aligned text
+    drawing.add(dxf.text("aligned Text", insert=(0, -3),
                             halign=dxfwrite.ALIGNED, alignpoint=(3, -3)))
-    drawing.add(engine.line((0,-6), (3,-6)))
-    drawing.add(engine.text("fitted Text", insert=(0, -6),
+    drawing.add(dxf.line((0,-6), (3,-6)))
+
+    # example for fitted text
+    drawing.add(dxf.text("fitted Text", insert=(0, -6),
                             halign=dxfwrite.FIT, alignpoint=(3, -6)))
-    drawing.add(engine.line((0,-9), (3,-9)))
-    drawing.add(engine.text("baseline_middle Text", insert=(0, -9),
+    drawing.add(dxf.line((0,-9), (3,-9)))
+
+    # example for baseline_middle text
+    drawing.add(dxf.text("baseline_middle Text", insert=(0, -9),
                             halign=dxfwrite.BASELINE_MIDDLE, alignpoint=(3, -9)))
+    # and save the drawing
     drawing.save()
 
-def rectangle(engine, x=0, y=0, w=1, h=1):
-    # dxf list can contain any dxf entity, that supports __dxf__()
-    rect = dxfwrite.DXFList()
-    rect.append(engine.line(start=(x, y), end=(x+w, y), color=1))
-    rect.append(engine.line(start=(x+w, y), end=(x+w, y+h), color=2))
-    rect.append(engine.line(start=(x+w, y+h), end=(x, y+h), color=3))
-    rect.append(engine.line(start=(x, y+h), end=(x, y), color=4))
-    return rect
-
-def polyline(engine):
-    points = [(20,20), (25,25), (25, 30), (20, 35, 10)]
-    pline = engine.polyline(color=3)
-    for num, point in enumerate(points):
-        width = .05
-        pline.add_vertex(point, startwidth=width, endwidth=width, bulge=-.2)
-    pline.close()
-    return pline
-
-def polymesh(engine):
-    mesh = engine.polymesh(3, 3)
-    mesh.set_vertex(0,0, (40, 10, 1))
-    mesh.set_vertex(0,1, (45, 10, 0))
-    mesh.set_vertex(0,2, (50, 10, 0))
-    mesh.set_vertex(1,0, (40, 15, 3))
-    mesh.set_vertex(1,1, (47, 13, 3))
-    mesh.set_vertex(1,2, (50, 15, 3))
-    mesh.set_vertex(2,0, (40, 20, 0))
-    mesh.set_vertex(2,1, (45, 20, 0))
-    mesh.set_vertex(2,2, (50, 20, 1))
-    return mesh
-
-def polyface(engine):
-    def rect(x, y, z, w, h):
-        """ create a rectangle of four vertices """
-        return [(x, y, z), (x+w, y, z), (x+w, y+h, z), (x, y+h, z)]
-    # a polyface consist of arbritary 3- or 4-sided faces
-    mesh = engine.polyface()
-    # a face is defined by three or four 3d-points.
-    mesh.add_face(rect(40, 10, 0, 10, 10))
-    mesh.add_face(rect(55, 10, 0, 10, 10))
-    mesh.add_face(rect(45, 15, 1, 10, 10))
-    return mesh
-
-def cube(engine, basepoint, length):
-    def scale( point ):
-        return ( (basepoint[0]+point[0]*length),
-                 (basepoint[1]+point[1]*length),
-                 (basepoint[2]+point[2]*length))
-    pface = engine.polyface()
-    # cube corner points
-    p1 = scale( (0,0,0) )
-    p2 = scale( (0,0,1) )
-    p3 = scale( (0,1,0) )
-    p4 = scale( (0,1,1) )
-    p5 = scale( (1,0,0) )
-    p6 = scale( (1,0,1) )
-    p7 = scale( (1,1,0) )
-    p8 = scale( (1,1,1) )
-
-    # define the 6 cube faces
-    # look into -x direction
-    # Every add_face adds 4 vertices 6x4 = 24 vertices
-    # On dxf output double vertices will be removed.
-    pface.add_face([p1, p5, p7, p3], color=1) # base
-    pface.add_face([p1, p5, p6, p2], color=2) # left
-    pface.add_face([p5, p7, p8, p6], color=3) # front
-    pface.add_face([p7, p8, p4, p3], color=4) # right
-    pface.add_face([p1, p3, p4, p2], color=5) # back
-    pface.add_face([p2, p6, p8, p4], color=6) # top
-    return pface
-
-def mtext_dxf(engine, name):
+def mtext_dxf(dxf, name):
+    """ shows the MText object """
     def textblock(mtext, x, y, rot, color=3, mirror=0):
-        dwg.add(engine.line((x+50, y), (x+50, y+50),  color=color))
-        dwg.add(engine.line((x+100, y), (x+100, y+50), color=color))
-        dwg.add(engine.line((x+150, y), (x+150, y+50), color=color))
+        dwg.add(dxf.line((x+50, y), (x+50, y+50),  color=color))
+        dwg.add(dxf.line((x+100, y), (x+100, y+50), color=color))
+        dwg.add(dxf.line((x+150, y), (x+150, y+50), color=color))
 
-        dwg.add(engine.line((x+50, y), (x+150, y), color=color))
-        dwg.add(engine.mtext(mtext, (x+50, y), mirror=mirror, rotation=rot))
-        dwg.add(engine.mtext(mtext, (x+100, y), mirror=mirror, rotation=rot, halign=dxfwrite.CENTER))
-        dwg.add(engine.mtext(mtext, (x+150, y), mirror=mirror, rotation=rot, halign=dxfwrite.RIGHT))
+        dwg.add(dxf.line((x+50, y), (x+150, y), color=color))
+        dwg.add(dxf.mtext(mtext, (x+50, y), mirror=mirror, rotation=rot))
+        dwg.add(dxf.mtext(mtext, (x+100, y), mirror=mirror, rotation=rot,
+                          halign=dxfwrite.CENTER))
+        dwg.add(dxf.mtext(mtext, (x+150, y), mirror=mirror, rotation=rot,
+                          halign=dxfwrite.RIGHT))
 
-        dwg.add(engine.line((x+50, y+25), (x+150, y+25), color=color))
-        dwg.add(engine.mtext(mtext, (x+50, y+25), mirror=mirror, rotation=rot, valign=dxfwrite.MIDDLE))
-        dwg.add(engine.mtext(mtext, (x+100, y+25), mirror=mirror, rotation=rot, valign=dxfwrite.MIDDLE, halign=dxfwrite.CENTER))
-        dwg.add(engine.mtext(mtext, (x+150, y+25), mirror=mirror, rotation=rot, valign=dxfwrite.MIDDLE, halign=dxfwrite.RIGHT))
+        dwg.add(dxf.line((x+50, y+25), (x+150, y+25), color=color))
+        dwg.add(dxf.mtext(mtext, (x+50, y+25), mirror=mirror, rotation=rot,
+                          valign=dxfwrite.MIDDLE))
+        dwg.add(dxf.mtext(mtext, (x+100, y+25), mirror=mirror, rotation=rot,
+                          valign=dxfwrite.MIDDLE, halign=dxfwrite.CENTER))
+        dwg.add(dxf.mtext(mtext, (x+150, y+25), mirror=mirror, rotation=rot,
+                          valign=dxfwrite.MIDDLE, halign=dxfwrite.RIGHT))
 
-        dwg.add(engine.line((x+50, y+50), (x+150, y+50), color=color))
-        dwg.add(engine.mtext(mtext, (x+50, y+50), mirror=mirror, valign=dxfwrite.BOTTOM, rotation=rot))
-        dwg.add(engine.mtext(mtext, (x+100, y+50), mirror=mirror, valign=dxfwrite.BOTTOM, rotation=rot, halign=dxfwrite.CENTER))
-        dwg.add(engine.mtext(mtext, (x+150, y+50), mirror=mirror, valign=dxfwrite.BOTTOM, rotation=rot, halign=dxfwrite.RIGHT))
+        dwg.add(dxf.line((x+50, y+50), (x+150, y+50), color=color))
+        dwg.add(dxf.mtext(mtext, (x+50, y+50), mirror=mirror,
+                          valign=dxfwrite.BOTTOM, rotation=rot))
+        dwg.add(dxf.mtext(mtext, (x+100, y+50), mirror=mirror,
+                          valign=dxfwrite.BOTTOM, rotation=rot,
+                          halign=dxfwrite.CENTER))
+        dwg.add(dxf.mtext(mtext, (x+150, y+50), mirror=mirror,
+                          valign=dxfwrite.BOTTOM, rotation=rot,
+                          halign=dxfwrite.RIGHT))
 
     def rotate_text(text, insert, parts=16, color=3):
         delta = 360. / parts
         for part in xrange(parts):
-            dwg.add(engine.mtext(text, insert, rotation=(delta*part),
+            dwg.add(dxf.mtext(text, insert, rotation=(delta*part),
                                  color=color, valign=dxfwrite.TOP))
 
-    dwg = engine.drawing(name)
-    mtext = "Das ist ein mehrzeiliger Text\nZeile 2\nZeile 3\nUnd eine lange lange" \
+    dwg = dxf.drawing(name)
+    txt = "Das ist ein mehrzeiliger Text\nZeile 2\nZeile 3\nUnd eine lange lange" \
             " ................ Zeile4"
 
-    textblock(mtext, 0, 0, 0., color=1)
-    textblock(mtext, 150, 0,  45., color=2)
-    textblock(mtext, 300, 0,  90., color=3)
+    textblock(txt, 0, 0, 0., color=1)
+    textblock(txt, 150, 0,  45., color=2)
+    textblock(txt, 300, 0,  90., color=3)
 
-    textblock(mtext, 0, 70,  135., color=4)
-    textblock(mtext, 150, 70,  180., color=5)
-    textblock(mtext, 300, 70,  225., color=6)
+    textblock(txt, 0, 70,  135., color=4)
+    textblock(txt, 150, 70,  180., color=5)
+    textblock(txt, 300, 70,  225., color=6)
 
-    mtext = "MText Zeile 1\nMIRROR_X\nZeile 3"
-    textblock(mtext, 0, 140,  0., color=4, mirror=dxfwrite.MIRROR_X)
-    textblock(mtext, 150, 140,  45., color=5, mirror=dxfwrite.MIRROR_X)
-    textblock(mtext, 300, 140,  90., color=6, mirror=dxfwrite.MIRROR_X)
+    txt = "MText Zeile 1\nMIRROR_X\nZeile 3"
+    textblock(txt, 0, 140,  0., color=4, mirror=dxfwrite.MIRROR_X)
+    textblock(txt, 150, 140,  45., color=5, mirror=dxfwrite.MIRROR_X)
+    textblock(txt, 300, 140,  90., color=6, mirror=dxfwrite.MIRROR_X)
 
-    mtext = "MText Zeile 1\nMIRROR_Y\nZeile 3"
-    textblock(mtext, 0, 210,  0., color=4, mirror=dxfwrite.MIRROR_Y)
-    textblock(mtext, 150, 210,  45., color=5, mirror=dxfwrite.MIRROR_Y)
-    textblock(mtext, 300, 210,  90., color=6, mirror=dxfwrite.MIRROR_Y)
+    txt = "MText Zeile 1\nMIRROR_Y\nZeile 3"
+    textblock(txt, 0, 210,  0., color=4, mirror=dxfwrite.MIRROR_Y)
+    textblock(txt, 150, 210,  45., color=5, mirror=dxfwrite.MIRROR_Y)
+    textblock(txt, 300, 210,  90., color=6, mirror=dxfwrite.MIRROR_Y)
 
     textblock("Einzeiler  0 deg", 0, -70, 0., color=1)
     textblock("Einzeiler 45 deg", 150, -70,  45., color=2)
     textblock("Einzeiler 90 deg", 300, -70,  90., color=3)
 
-    mtext = "--------------------------------------------------Zeile 1\n" \
-            "----------------- MTEXT MTEXT --------------------Zeile 2 zum Rotieren!\n" \
-            "--------------------------------------------------Zeile 3\n"
-    rotate_text(mtext, (600, 100), parts=16, color=3)
+    txt = "--------------------------------------------------Zeile 1\n" \
+          "----------------- MTEXT MTEXT --------------------Zeile 2 zum Rotieren!\n" \
+          "--------------------------------------------------Zeile 3\n"
+    rotate_text(txt, (600, 100), parts=16, color=3)
     dwg.save()
 
-def dimline_dxf(engine, filename):
+def rectangle_dxf(dxf, name):
+    """ show the Rectangle object """
+    drawing = dxf.drawing(name)
+
+    for x in range(10):
+        for y in range(10):
+            color = 255 * random()
+            bgcolor = 255 * random()
+            rand = random()
+            # rectangle with only backgound filling
+            drawing.add(dxf.rectangle((x*3, y*3) , 1.5*rand, .7*rand,
+                                      bgcolor=bgcolor))
+            angle = 90 * random()
+            # rectangle with only border lines
+            drawing.add(dxf.rectangle((40+(x*3), y*3) , 1.5*rand, .7*rand,
+                                      color=color, rotation=angle))
+    drawing.save()
+
+def dimline_dxf(dxf, filename):
     # Dimlines are separated from the core library.
     # Dimension lines will not generated by the DXFEngine.
 
@@ -232,7 +229,7 @@ def dimline_dxf(engine, filename):
     from dxfwrite.dimlines import ArcDimension, RadialDimension
 
     # create a new drawing: dxfwrite.DXFEngine.drawing(filename)
-    dwg = engine.drawing(filename)
+    dwg = dxf.drawing(filename)
 
     # add block and layer definition to drawing
     dimstyles.setup(dwg)
@@ -258,7 +255,7 @@ def dimline_dxf(engine, filename):
     dwg.add_anonymous_block(dimline, layer='DIMENSIONS')
 
     # add polyline to drawing
-    dwg.add(engine.polyline(points, color=5))
+    dwg.add(dxf.polyline(points, color=5))
 
     # There are three dimstyle presets for angular dimension
     # 'angle.deg' (default), 'angle.rad', 'angle.grad' (gon)
@@ -281,11 +278,11 @@ def dimline_dxf(engine, filename):
     dwg.add(RadialDimension((20, 0), (24, 1.5), dimstyle='radius'))
     dwg.save()
 
-def get_3d_entities(engine):
+def get_cos_sin_mesh(dxf):
     msize = 20
     height = 3.
     # create a new polymesh (m*n), here m=n
-    mesh = engine.polymesh(msize, msize)
+    mesh = dxf.polymesh(msize, msize)
     delta = math.pi / msize
     for x in range(msize):
         sinx = math.sin(float(x)*delta)
@@ -294,10 +291,10 @@ def get_3d_entities(engine):
             z = sinx * cosy * height
             # set the m,n vertex to 3d point x,y,z
             mesh.set_vertex(x, y, (x, y, z))
-    return [mesh]
+    return mesh
 
-def models3d_dxf(engine, name):
-    dwg = engine.drawing(name) # create a drawing
+def polymesh_dxf(dxf, name):
+    dwg = dxf.drawing(name) # create a drawing
     dwg.add_viewport( # add the active viewport
         '*Active',
         center_point=(0,0),
@@ -305,16 +302,59 @@ def models3d_dxf(engine, name):
         direction_point=(30,30,10)
         )
 
-    for dxf_obj in get_3d_entities(engine):
-        dwg.add(dxf_obj) # add dxf objects to drawing
+    dwg.add(get_cos_sin_mesh(dxf)) # add dxf objects to drawing
+    dwg.save() # save dxf drawing
+
+def get_cube(dxf, basepoint, length):
+    def scale( point ):
+        return ( (basepoint[0]+point[0]*length),
+                 (basepoint[1]+point[1]*length),
+                 (basepoint[2]+point[2]*length))
+    pface = dxf.polyface()
+    # cube corner points
+    p1 = scale( (0,0,0) )
+    p2 = scale( (0,0,1) )
+    p3 = scale( (0,1,0) )
+    p4 = scale( (0,1,1) )
+    p5 = scale( (1,0,0) )
+    p6 = scale( (1,0,1) )
+    p7 = scale( (1,1,0) )
+    p8 = scale( (1,1,1) )
+
+    # define the 6 cube faces
+    # look into -x direction
+    # Every add_face adds 4 vertices 6x4 = 24 vertices
+    # On dxf output double vertices will be removed.
+    pface.add_face([p1, p5, p7, p3], color=1) # base
+    pface.add_face([p1, p5, p6, p2], color=2) # left
+    pface.add_face([p5, p7, p8, p6], color=3) # front
+    pface.add_face([p7, p8, p4, p3], color=4) # right
+    pface.add_face([p1, p3, p4, p2], color=5) # back
+    pface.add_face([p2, p6, p8, p4], color=6) # top
+    return pface
+
+def polyface_dxf(dxf, name):
+    dwg = dxf.drawing(name) # create a drawing
+    dwg.add_viewport( # add the active viewport
+        '*Active',
+        center_point=(0,0),
+        height = 30,
+        direction_point=(30,30,10)
+        )
+
+    for x in xrange(10):
+        for y in xrange(10):
+            dwg.add(get_cube(dxf, (x,y, random()), random()))
     dwg.save() # save dxf drawing
 
 def main():
-    empty_dxf(dxfwrite.DXFEngine, "empty.dxf")
-    simple_dxf(dxfwrite.DXFEngine, "simple.dxf")
-    mtext_dxf(dxfwrite.DXFEngine, "mtext.dxf")
-    dimline_dxf(dxfwrite.DXFEngine, "dimlines.dxf")
-    models3d_dxf(dxfwrite.DXFEngine, "models3d.dxf")
+    empty_dxf(dxfwrite.DXFEngine, "example_empty.dxf")
+    simple_dxf(dxfwrite.DXFEngine, "example_simple.dxf")
+    mtext_dxf(dxfwrite.DXFEngine, "example_mtext.dxf")
+    rectangle_dxf(dxfwrite.DXFEngine, "example_rectangle.dxf")
+    dimline_dxf(dxfwrite.DXFEngine, "example_dimlines.dxf")
+    polymesh_dxf(dxfwrite.DXFEngine, "example_polymesh.dxf")
+    polyface_dxf(dxfwrite.DXFEngine, "example_polyface.dxf")
 
 if __name__=='__main__':
     main()
